2e44e5918b6fed73ea6193a9332c93db0b602bf0
Armando <amezal01@hotmail.com>
2022-03-24 16:59:44 -0600

    fixed Equipo carousel

--- src/components/sobre-nosotros/Equipo.js
+++ src/components/sobre-nosotros/Equipo.js
@@ -1,10 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { StaticImage } from 'gatsby-plugin-image';
 
 const Equipo = () => {
-
-  const numberOfPages = 2;
-  const [active, setActive] = useState(0);
 
   const equipo = [
     {
@@ -28,12 +25,41 @@ const Equipo = () => {
       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing.'
     },
   ]
+  const [active, setActive] = useState(0);
+  const [visibleItems, setVisibleItems] = useState(3);
+  const [numberOfPages, setNumberOfPages] = useState(2);
+  const carouselRef = useRef(null);
+
+  const setSlides = () => {
+    let visible = 3;
+    if (window.outerWidth <= 568) { visible = 1; console.log(equipo.length - visible + 1) }
+    else if (window.outerWidth <= 1024) { visible = 2 }
+    else { visible = 3 }
+
+    //number of pages
+    const nop = equipo.length - visible + 1;
+    setVisibleItems(visible);
+    setNumberOfPages(nop);
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', setSlides);
+    return (() => window.removeEventListener('resize', setSlides));
+  }, [])
+
+  const goToSlide = (i) => {
+    setActive(i);
+    const width = carouselRef.current.children[0].getBoundingClientRect().width;
+    carouselRef.current.scrollTo({ left: width * i, behavior: 'smooth' })
+
+  }
+
 
   return (
     <section className="equipo">
       <div className="container">
         <h2>El equipo</h2>
-        <div className="equipo__carousel">
+        <div className="equipo__carousel" ref={carouselRef}>
           {equipo.map(integrante => (
             <div className="integrante" key={integrante.name}>
               <img
@@ -49,7 +75,7 @@ const Equipo = () => {
             [...Array(numberOfPages)].map((btn, i) => (
               <button
                 key={i}
-                onClick={() => setActive(i)}
+                onClick={() => goToSlide(i)}
                 className={active === i ? 'active' : ''}
               >
                 <div></div>
