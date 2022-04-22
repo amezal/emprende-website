22e906b9e72b676cd74686f560401e05014c89fe
Armando <amezal01@hotmail.com>
2022-04-22 15:15:12 -0600

    display events in calendar

--- src/components/eventos/Calendario.js
+++ src/components/eventos/Calendario.js
@@ -1,25 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import Calendar from 'react-calendar';
-import { formatISO } from 'date-fns'
 import moment from 'moment';
 
 const Calendario = ({ events }) => {
-  const [date, setDate] = useState(new Date());
-  // const [dates, setDates] = useState(null);
-  const dates = events.map(event => event.start.dateTime)
-  console.log(dates);
+  const [displayedEvent, setDisplayedEvent] = useState(events[0]);
+  console.log(moment(displayedEvent.start.dateTime).format('dddd'));
 
-
-  const changeDate = (e) => {
-    const x = moment(e).isSame(new Date(), 'day');
-    console.log(x);
+  const changeDate = (date) => {
+    const event = events.find(event => moment(date).isSame(event.start.dateTime, 'day'))
+    if (event) {
+      setDisplayedEvent(event);
+    }
+    else {
+      setDisplayedEvent({
+        start: {
+          dateTime: date
+        },
+        summary: 'Sin eventos'
+      })
+    }
   }
 
   function tileClassName({ date, view }) {
+    const dates = events.map(event => event.start.dateTime)
     if (view === 'month') {
-      // dates.forEach(dDate => console.log(moment(dDate).isSame(date, 'day')))
       if (dates.find(dDate => moment(dDate).isSame(date, 'day'))) {
-        console.log('hola');
         return 'event-days';
       }
     }
@@ -30,9 +35,9 @@ const Calendario = ({ events }) => {
       <div className="container">
         <div className="event-calendar">
           <div className="event">
-            <h3>21</h3>
-            <h2>Jueves</h2>
-            <button>Feria Metrocentro</button>
+            <h3>{moment(displayedEvent.start.dateTime).format('D')}</h3>
+            <h2>{moment(displayedEvent.start.dateTime).format('dddd')}</h2>
+            <button>{displayedEvent.summary}</button>
           </div>
           <Calendar
             locale="es-es"
