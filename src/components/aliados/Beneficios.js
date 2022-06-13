db38af608d04082683b4d72e9329540147392a6e
Armando <amezal01@hotmail.com>
2022-06-12 22:55:53 -0600

    beneficios updated

--- src/components/aliados/Beneficios.js
+++ src/components/aliados/Beneficios.js
@@ -1,22 +1,45 @@
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const Beneficios = () => {
   return (
     <section className="beneficios">
       <div className="container">
-        <h2>Beneficios</h2>
+        <h2>Aliados Emprende</h2>
+        <p>
+          Convertite en una empresa aliada para juntos potenciar el progreso económico en  la región.
+        </p>
+        <h3>Beneficios para tu marca</h3>
         <div className="beneficios__list">
           <div className="beneficio">
-            <img src="https://picsum.photos/100/100" alt="beneficio" />
-            <p>Te posicionarás como una marca con propósito, comprometida con la sostenibilidad y las personas</p>
+            <StaticImage
+              src='../../images/Beneficios01.png'
+              quality={100}
+              className='beneficio__img'
+              width={150}
+            />
+            <h4>Posicionamiento</h4>
+            <p>Tu marca comprometida con la sostenibilidad y las personas</p>
           </div>
           <div className="beneficio">
-            <img src="https://picsum.photos/100/100" alt="beneficio" />
-            <p>Tendrás una conexión más cercana con tus clientes, ya que promovemos a emprendimientos de diferentes industrias</p>
+            <StaticImage
+              src='../../images/Beneficios02.png'
+              quality={100}
+              className='beneficio__img'
+              width={150}
+            />
+            <h4>Conexión</h4>
+            <p>Con el segmento de mercado emprendedor</p>
           </div>
           <div className="beneficio">
-            <img src="https://picsum.photos/100/100" alt="beneficio" />
-            <p>Ganarás promoción y alcance de marca por medio de nuestras menciones en medios impresos y digitales</p>
+            <StaticImage
+              src='../../images/Beneficios03.png'
+              quality={100}
+              className='beneficio__img'
+              width={150}
+            />
+            <h4>Alcance</h4>
+            <p>Por medio de nuestras menciones en medios impresos y digitales</p>
           </div>
         </div>
       </div>
