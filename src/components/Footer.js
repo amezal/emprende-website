9174ac3a90e9a3106bea5e12d392c04e4ab8d037
Armando <amezal01@hotmail.com>
2022-06-23 09:44:13 -0600

    cambios footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
     <footer className="footer">
       <div className="container">
         <div className="footer__left">
-          <StaticImage src="../images/Logo1.png" height="81"
+          <StaticImage src="../images/Logo1.png" height="70"
             loading="eager" alt="logo" className="img"
             placeholder='none' quality="100" formats={["png"]} />
           <h3>¿Querés promover el <br /> progreso económico <br /> de la región?</h3>
@@ -19,10 +19,10 @@ const Footer = () => {
         <div className="footer__right">
           <Link to="/contacto">Contactanos</Link>
           <div className="footer__right__socials">
-            <a target="_blank" rel="noreferrer" href="https://www.instagram.com/emprende.ca/"><FaInstagram size="30px" /> </a>
-            <a target="_blank" rel="noreferrer" href="https://www.facebook.com/emprender.ca"><FaFacebookSquare size="30px" /> </a>
-            <a target="_blank" rel="noreferrer" href="https://www.tiktok.com/@emprende.ca"><FaTiktok size="30px" /> </a>
-            <a target="_blank" rel="noreferrer" href="https://www.youtube.com/channel/UCa7zHM8AdmiUGJhMK7f2TLw"><FaYoutube size="30px" /> </a>
+            <a target="_blank" rel="noreferrer" href="https://www.instagram.com/emprende.ca/"><FaInstagram size="25px" /> </a>
+            <a target="_blank" rel="noreferrer" href="https://www.facebook.com/emprender.ca"><FaFacebookSquare size="25px" /> </a>
+            <a target="_blank" rel="noreferrer" href="https://www.tiktok.com/@emprende.ca"><FaTiktok size="25px" /> </a>
+            <a target="_blank" rel="noreferrer" href="https://www.youtube.com/channel/UCa7zHM8AdmiUGJhMK7f2TLw"><FaYoutube size="25px" /> </a>
           </div>
           <p>2022 Programa Emprende. Todos los derechos reservados.</p>
           <b>Desarrollado por: Sidekick & Armando Meza</b>
